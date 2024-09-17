79cc3bedf252bee00e87601ea1244cf0820377d4
ms-jade03 <ms_jade_03_0501@yahoo.co.jp>
2024-09-17 19:00:42 +0900

    index.blade.phpを編集

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -28,6 +28,8 @@
           <p>{{ session('flash_message') }}</p>
       @endif
 
+      <a href="{{ route('posts.create') }}">新規投稿</a>
+
       @if($posts->isNotEmpty())
         @foreach($posts as $post)
           <article>
