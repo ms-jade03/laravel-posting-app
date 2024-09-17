036d352e5b8f287a7074736539ad51fcc2b29cb0
ms-jade03 <ms_jade_03_0501@yahoo.co.jp>
2024-09-17 20:25:17 +0900

    PostController.phpを編集

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -37,4 +37,13 @@ class PostController extends Controller
 
         return redirect()->route('posts.index')->with('flash_message', '投稿が完了しました。');
      }
+
+     public function edit(Post $post)
+     {
+         if ($post->user_id !== Auth::id()) {
+             return redirect()->route('posts.index')->with('error_message', '不正なアクセスです。');
+         }
+ 
+         return view('posts.edit', compact('post'));
+     }
 }
